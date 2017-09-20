28ee9f2715b6ca60d6af7dde8d4fc82048db18f9
Etienne <fermivor@gmail.com>
2017-09-20 17:10:08 +0200

    movement2

--- Assets/Scripts/IPlayerController.cs
+++ Assets/Scripts/IPlayerController.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class IPlayerController : MonoBehaviour {
+public class IPlayerController : NetworkBehaviour {
+
+	[SerializeField]
+	private float _Speed;
+
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +16,37 @@ public class IPlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+
+		// Handle simple movement
+		Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 playerPos = transform.position;
+
+		if(Vector2.Distance(mousePos, playerPos) > 0.05f)
+		{
+			Vector2 direction = mousePos - playerPos;
+			direction.Normalize();
+
+			if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z))
+			{
+				GetComponent<Rigidbody2D>().velocity = direction * _Speed;
+			}
+
+			Vector3 movingDir = new Vector3(direction.x, direction.y, 0f);
+			float angle = Mathf.Atan2(movingDir.y, movingDir.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		}
+		else
+		{
+			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		}
+
+		if (Input.GetKeyUp(KeyCode.Z) || Input.GetKeyUp(KeyCode.UpArrow))
+		{
+			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		}
 	}
 }
