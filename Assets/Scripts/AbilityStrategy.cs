dfc2a98c96530bcbcdf2387faa50589c2f51282b
Etienne <fermivor@gmail.com>
2017-09-29 17:01:06 +0200

    movement

--- Assets/Scripts/AbilityStrategy.cs
+++ Assets/Scripts/AbilityStrategy.cs
@@ -5,7 +5,36 @@ using UnityEngine;
 public abstract class AbilityStrategy : MonoBehaviour {
 
 	// Movement
-	public virtual void PlayerMovement(GameObject iPlayer){}
+	public virtual void PlayerMovement(GameObject iPlayer)
+	{
+		// Handle simple movement
+		Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 playerPos = transform.position;
+
+		if(Vector2.Distance(mousePos, playerPos) > 0.05f)
+		{
+			Vector2 direction = mousePos - playerPos;
+			direction.Normalize();
+
+			if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z))
+			{
+				GetComponent<Rigidbody2D>().velocity = direction * _Speed;
+			}
+
+			Vector3 movingDir = new Vector3(direction.x, direction.y, 0f);
+			float angle = Mathf.Atan2(movingDir.y, movingDir.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		}
+		else
+		{
+			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		}
+
+		if (Input.GetKeyUp(KeyCode.Z) || Input.GetKeyUp(KeyCode.UpArrow))
+		{
+			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		}
+	}
 
 	// Ability1
 	public virtual void Ability1(){}
--- Assets/Scripts/IPlayerController.cs
+++ Assets/Scripts/IPlayerController.cs
@@ -8,11 +8,9 @@ public class IPlayerController : NetworkBehaviour {
 	[SerializeField]
 	private float _Speed;
 
+	private AbilityStrategy _Strat;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,33 +18,8 @@ public class IPlayerController : NetworkBehaviour {
 		{
 			return;
 		}
+	
+		_Strat.PlayerMovement(gameObject);
 
-		// Handle simple movement
-		Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		Vector2 playerPos = transform.position;
-
-		if(Vector2.Distance(mousePos, playerPos) > 0.05f)
-		{
-			Vector2 direction = mousePos - playerPos;
-			direction.Normalize();
-
-			if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z))
-			{
-				GetComponent<Rigidbody2D>().velocity = direction * _Speed;
-			}
-
-			Vector3 movingDir = new Vector3(direction.x, direction.y, 0f);
-			float angle = Mathf.Atan2(movingDir.y, movingDir.x) * Mathf.Rad2Deg;
-			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-		}
-		else
-		{
-			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-		}
-
-		if (Input.GetKeyUp(KeyCode.Z) || Input.GetKeyUp(KeyCode.UpArrow))
-		{
-			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-		}
 	}
 }
