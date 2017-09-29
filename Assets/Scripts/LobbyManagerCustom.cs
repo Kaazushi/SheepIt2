5c895b454d4014b5614d819394949f44a41e8e6c
Zeldarck <zeldarck.martin@gmail.com>
2017-09-29 16:56:18 +0200

    LobyManagerCustom

---
+++ Assets/Scripts/LobbyManagerCustom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LobbyManagerCustom : Prototype.NetworkLobby.LobbyManager
+{
+    List<NetworkConnection> m_clients = new List<NetworkConnection>();
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+        m_clients.Add(conn);
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        base.OnClientDisconnect(conn);
+        m_clients.Remove(conn);
+
+    }
+
+}
