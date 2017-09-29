62fe5fce92258d211f17f224f3877383136a6641
Etienne <fermivor@gmail.com>
2017-09-29 16:21:47 +0200

    qsdfghj

---
+++ Assets/Scripts/IAbilityStrategy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class IAbilityStrategy : MonoBehaviour {
+
+	// Movement
+	private void PlayerMovement(GameObject iPlayer);
+
+	// Ability1
+	private void Ability1();
+
+	// Ability2
+	private void Ability2();
+
+	// Death
+	private void PlayerDeath();
+}
---
+++ Assets/Scripts/SheepStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepStrategy : IAbilityStrategy {
+
+	// Movement
+	private override void PlayerMovement();
+
+	// Ability1
+	private override void Ability1();
+
+	// Ability2
+	private override void Ability2();
+}
