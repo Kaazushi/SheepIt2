2f30bc267cecc32b0895e2db0ebe00ef0f5c73bf
Zeldarck <zeldarck.martin@gmail.com>
2017-10-12 14:56:00 +0200

    change gameobject to primitive rpc/command

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,12 +6,12 @@ using UnityEngine.Networking;
 
 public enum AnimalType { SHEEP, WOLF}
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
 
     public static GameManager INSTANCE;
     GameObject[] m_players;
-    Dictionary<GameObject, int> m_points = new Dictionary<GameObject, int>();
+    Dictionary<NetworkInstanceId, int> m_points = new Dictionary<NetworkInstanceId, int>();
     int m_preda = -1;
 
     void Start()
@@ -34,10 +34,12 @@ public class GameManager : MonoBehaviour
 
     public void BeginGame()
     {
+       
         m_players = GameObject.FindGameObjectsWithTag("Player");
         foreach (GameObject go in m_players)
         {
-            m_points[go] = 0;
+            
+            m_points[go.GetComponent<NetworkIdentity>().netId] = 0;
         }
         m_preda = -1;
         StartRound();
@@ -53,25 +55,22 @@ public class GameManager : MonoBehaviour
         }
         for(int i = 0; i < m_players.Length; i++)
         {
-            Skin skin;
-            AbilityStrategy strat;
+            AnimalType type;
             if(i == m_preda)
             {
-                skin = SkinFactory.INSTANCE.getSkin(AnimalType.WOLF);
-                strat = AbilityStrategyFactory.INSTANCE.getAbilityStrategy(AnimalType.WOLF);
+                type = AnimalType.WOLF;
                 //m_players[m_preda].GetComponent<IPlayerController>().SetPreda(true);
             }
             else
             {
-                skin = SkinFactory.INSTANCE.getSkin(AnimalType.SHEEP);
-                strat = AbilityStrategyFactory.INSTANCE.getAbilityStrategy(AnimalType.SHEEP);
+                type = AnimalType.SHEEP;
             }
-            m_players[i].GetComponent<IPlayerController>().RpcSetSkin(skin,strat);
+            m_players[i].GetComponent<IPlayerController>().RpcSetSkin(type);
         }
     }
 
     [Command]
-    void CmdAddPoint(GameObject a_player)
+    void CmdAddPoint(NetworkInstanceId a_player)
     {
         m_points[a_player]++;
         if (m_points[a_player] == m_players.Length -1)
