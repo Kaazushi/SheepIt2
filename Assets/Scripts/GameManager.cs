08005b30f054642888f6a619e9fdbd57a8b40901
Zeldarck <zeldarck.martin@gmail.com>
2017-10-12 14:44:25 +0200

    gamemanager count point

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,18 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public enum AnimalType { SHEEP, WOLF}
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
     public static GameManager INSTANCE;
-    GameObject[] m_players ;
+    GameObject[] m_players;
     Dictionary<GameObject, int> m_points = new Dictionary<GameObject, int>();
     int m_preda = -1;
 
-    void Start () {
-		if(INSTANCE != null && INSTANCE != this)
+    void Start()
+    {
+        if (INSTANCE != null && INSTANCE != this)
         {
             DestroyImmediate(gameObject);
         }
@@ -22,16 +25,17 @@ public class GameManager : MonoBehaviour {
             INSTANCE = this;
             DontDestroyOnLoad(this);
         }
-	}
-	
+    }
 
-	void Update () {
-	}
+
+    void Update()
+    {
+    }
 
     public void BeginGame()
     {
         m_players = GameObject.FindGameObjectsWithTag("Player");
-        foreach(GameObject go in m_players)
+        foreach (GameObject go in m_players)
         {
             m_points[go] = 0;
         }
@@ -42,12 +46,38 @@ public class GameManager : MonoBehaviour {
     private void StartRound()
     {
         m_preda++;
-        if(m_preda >= m_players.Length)
+        if (m_preda >= m_players.Length)
         {
             //tout le monde a été prédateur
             return;
         }
-        m_players[m_preda].GetComponent<IPlayerController>().SetSkin(SkinFactory.INSTANCE.getSkin(AnimalType.WOLF), AbilityStrategyFactory.INSTANCE.getAbilityStrategy(AnimalType.WOLF));
-        //m_players[m_preda].GetComponent<IPlayerController>().SetPreda(true);
+        for(int i = 0; i < m_players.Length; i++)
+        {
+            Skin skin;
+            AbilityStrategy strat;
+            if(i == m_preda)
+            {
+                skin = SkinFactory.INSTANCE.getSkin(AnimalType.WOLF);
+                strat = AbilityStrategyFactory.INSTANCE.getAbilityStrategy(AnimalType.WOLF);
+                //m_players[m_preda].GetComponent<IPlayerController>().SetPreda(true);
+            }
+            else
+            {
+                skin = SkinFactory.INSTANCE.getSkin(AnimalType.SHEEP);
+                strat = AbilityStrategyFactory.INSTANCE.getAbilityStrategy(AnimalType.SHEEP);
+            }
+            m_players[i].GetComponent<IPlayerController>().RpcSetSkin(skin,strat);
+        }
     }
+
+    [Command]
+    void CmdAddPoint(GameObject a_player)
+    {
+        m_points[a_player]++;
+        if (m_points[a_player] == m_players.Length -1)
+        {
+            StartRound();
+        }
+    }
+
 }
