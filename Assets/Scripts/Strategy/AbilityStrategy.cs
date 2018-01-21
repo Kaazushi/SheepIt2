53cc30a7b91ace43e64227328a207b9fa3a1bb1e
Zeldarck <zeldarck.martin@gmail.com>
2018-01-21 14:08:47 +0100

    Update to handle fear

--- Assets/Scripts/Strategy/AbilityStrategy.cs
+++ Assets/Scripts/Strategy/AbilityStrategy.cs
@@ -7,53 +7,93 @@ public abstract class AbilityStrategy : MonoBehaviour {
 
 	[SerializeField]
 	private float _Speed = 5;
-    int m_fixedSpeed = 0;
-    Vector3 m_fixedDirection;
+
+    protected GameObject m_player;
+
+    Vector2 m_fearDirection;
+    int m_fearSpeed = 0;
+    Timer m_fearTimer;
+
+    bool m_isInit = false;
+
+    public void Init( GameObject a_player)
+    {
+        m_player = a_player;
+        m_fearTimer = TimerFactory.INSTANCE.getTimer();
+
+        m_isInit = true;
+    }
+
+
+    //Give direction of movement
+    Vector2 GetDirection()
+    {
+        if (!m_isInit) return Vector2.zero;
+        Vector2 result = Vector2.zero;
+        if (m_fearTimer && m_fearTimer.IsTimerRunning())
+        {
+            result =  m_fearDirection;
+        }
+        else
+        {
+            // Handle simple movement
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 playerPos = m_player.transform.position;
+            if (Vector2.Distance(mousePos, playerPos) > 0.05f )
+            {
+
+                result = mousePos - playerPos;
+            }
+
+        }
+        result.Normalize();
+
+        return result;
+    }
+
+    //give speed
+    float GetSpeed()
+    {
+        if (!m_isInit) return 0;
+
+        float result = 0;
+        if (m_fearTimer && m_fearTimer.IsTimerRunning())
+        {
+            result = m_fearSpeed;
+        }
+        else
+        {
+            if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z)))
+            {
+                result = _Speed;
+            }
+
+        }
+        return result;
+    }
+
+
 
     // Movement
-    public virtual void PlayerMovement(GameObject iPlayer)
+    public virtual void PlayerMovement()
 	{
-		// Handle simple movement
-		Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		Vector2 playerPos = iPlayer.transform.position;
+        if (!m_isInit) return;
 
-		if(Vector2.Distance(mousePos, playerPos) > 0.05f)
-		{
+        Vector2 direction = GetDirection();
+        m_player.GetComponent<Rigidbody2D>().velocity = direction * GetSpeed();
+		Vector3 movingDir = new Vector3(direction.x, direction.y, 0f);
+		float angle = Mathf.Atan2(movingDir.y, movingDir.x) * Mathf.Rad2Deg;
+        m_player.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-			Vector2 direction = m_fixedSpeed != 0 ? new Vector2(m_fixedDirection.x, m_fixedDirection.y) : mousePos - playerPos;
-			direction.Normalize();
-
-            Debug.Log(direction);
-
-			if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z))
-			{
-				iPlayer.GetComponent<Rigidbody2D>().velocity = direction * _Speed;
-			}
-
-			Vector3 movingDir = new Vector3(direction.x, direction.y, 0f);
-			float angle = Mathf.Atan2(movingDir.y, movingDir.x) * Mathf.Rad2Deg;
-			iPlayer.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-		}
-		else
-		{
-			iPlayer.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-		}
-
-		if (!Input.GetKey(KeyCode.Z) && !Input.GetKey(KeyCode.UpArrow))
-		{
-			iPlayer.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-			iPlayer.GetComponent<Rigidbody2D>().angularVelocity = 0;
-			//iPlayer.GetComponent<Rigidbody2D>().angularDrag = 1;
-        }
 	}
 
 	// Ability1
-	public virtual void Ability1(GameObject iPlayer) {}
+	public virtual void Ability1() {}
 
     public void ForcePath(Vector3 a_position, int a_speed, int a_time)
     {
-        m_fixedDirection = -1 *(a_position - gameObject.transform.position);
-        m_fixedSpeed = a_speed;
+        m_fearDirection = -1 *(a_position - gameObject.transform.position);
+        m_fearSpeed = a_speed;
     }
 
 
